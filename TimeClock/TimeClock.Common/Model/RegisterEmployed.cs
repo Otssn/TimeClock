98ad306a4db3efc59dbe654654b362b21467598a
Otssn <88810729+Otssn@users.noreply.github.com>
2021-09-01 14:43:24 -0500

    Add azure functions

---
+++ TimeClock/TimeClock.Common/Model/RegisterEmployed.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeClock.Common.Model
+{
+    public class RegisterEmployed
+    {
+        public int Id { get; set; }
+
+        public DateTime createDate { get; set; }
+
+        public int type { get; set; }
+
+        public bool consolidated { get; set; }
+    }
+}
